6ad31155b6912eaeadb4f3ef39c50a931bd4bdbe
vaibhav kumar jha <vaibhavkumarjha46@gmail.com>
2022-05-28 23:24:37 +0530

    API Endpoint-1

---
+++ routes/addresses.routes.js
@@ -0,0 +1,8 @@
+// module.exports = (app) =>{
+//     var router = require("express").Router();
+//     var address = require("../controllers/Address.controller");
+
+//     app.post("/addresses", address.addAddress);
+
+//     app.use("/", router);
+// }
